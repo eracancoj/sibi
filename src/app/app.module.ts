d042919dede2af8bbe7543534bb8b8122ec91219
eracancoj <emersonfernando.rl@gmail.com>
2023-10-27 15:35:57 -0600

    firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    provideFirebaseApp(() =>
+      initializeApp({
+        projectId: 'sibipies',
+        appId: '1:103724154494:web:dbde61af3ac750b1da622f',
+        storageBucket: 'sibipies.appspot.com',
+        apiKey: 'AIzaSyACmVy4AP5ensl2kJWoIIn1_noQyVK6RSk',
+        authDomain: 'sibipies.firebaseapp.com',
+        messagingSenderId: '103724154494',
+      })
+    ),
+    provideFirestore(() => getFirestore()),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
